aa73fc76212921def0a8bffffdbae70d200a7e97
Mohit Kumar Bharti <mohitkumarbharti@Mac.lan>
2026-05-27 16:04:12 +0530

    added exercise and learning: :

---
+++ concurrency/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a placeholder for the main function.
+	// You can implement concurrency examples here.
+	fmt.Println("Concurrency examples will go here.")
+}
